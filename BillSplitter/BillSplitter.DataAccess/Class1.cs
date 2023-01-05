927955054d78de4553b0638a1bcc761c1753801a
Exodus <kubagr2000@gmail.com>
2023-01-05 13:34:30 +0100

    Add template project

---
+++ BillSplitter/BillSplitter.DataAccess/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace BillSplitter.DataAccess
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ BillSplitter/BillSplitter/Form1.cs
@@ -0,0 +1,10 @@
+namespace BillSplitter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
