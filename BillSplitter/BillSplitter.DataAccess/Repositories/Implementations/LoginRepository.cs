215597e3f2c0cf17192f782a50621148a62e69f8
Kamil K <53128417+craksys@users.noreply.github.com>
2023-01-24 16:15:55 +0100

    Hash passowrds (#3)
    * Fix indentation in LoginRepository.cs
    
    * Added hashing password
    
    * Update create_user.php
    
    * fix

--- BillSplitter/BillSplitter.DataAccess/Repositories/Implementations/LoginRepository.cs
+++ BillSplitter/BillSplitter.DataAccess/Repositories/Implementations/LoginRepository.cs
@@ -4,56 +4,65 @@ using System.Data;
 using Dapper;
 using BillSplitter.DataAccess.Models;
 using BillSplitter.DataAccess.Repositories.Interfaces;
+using System.Security.Cryptography;
 
 namespace BillSplitter.DataAccess.Repositories.Implementations;
 public class LoginRepository : ILoginRepository
 {
-    public LoginResult TryToLogin(string login, string password)
+  public LoginResult TryToLogin(string login, string password)
+  {
+
+    string? retrievedPassword;
+    try
     {
-
-        string? retrievedPassword;
-        try
-        {
-            retrievedPassword = GetUserPassword(login);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            return LoginResult.ConnectionError;
-        }
-
-        if (retrievedPassword is null)
-        {
-            return LoginResult.WrongCredentials;
-        }
-
-        if (retrievedPassword.Equals(password))
-        {
-            return LoginResult.Ok;
-        }
-
-        return LoginResult.WrongCredentials;
+      retrievedPassword = GetUserPassword(login);
+    }
+    catch (Exception e)
+    {
+      Console.WriteLine(e);
+      return LoginResult.ConnectionError;
     }
 
-    public User? GetUserByUserName(string login)
+    if (retrievedPassword is null)
     {
-        using var connection = ConnectionFactory.Create();
-
-        return connection.Query<User>(
-            StoredProcedures.GetUserByUsername,
-            new { user_name = login },
-            commandType: CommandType.StoredProcedure)
-          .FirstOrDefault();
+      return LoginResult.WrongCredentials;
     }
 
-    private static string? GetUserPassword(string login)
+    if (retrievedPassword.Equals(HashPassword(password)))
     {
-        using var connection = ConnectionFactory.Create();
-
-        return connection.Query<string>(
-            StoredProcedures.GetUsersPassword,
-            new { user_name = login },
-            commandType: CommandType.StoredProcedure)
-          .FirstOrDefault();
+      return LoginResult.Ok;
     }
+
+    return LoginResult.WrongCredentials;
+  }
+
+  public User? GetUserByUserName(string login)
+  {
+    using var connection = ConnectionFactory.Create();
+
+    return connection.Query<User>(
+        StoredProcedures.GetUserByUsername,
+        new { user_name = login },
+        commandType: CommandType.StoredProcedure)
+      .FirstOrDefault();
+  }
+
+  private static string? GetUserPassword(string login)
+  {
+    using var connection = ConnectionFactory.Create();
+
+    return connection.Query<string>(
+        StoredProcedures.GetUsersPassword,
+        new { user_name = login },
+        commandType: CommandType.StoredProcedure)
+      .FirstOrDefault();
+  }
+  
+  private string HashPassword(string password)
+  {
+    using var sha256 = SHA256.Create();
+    
+    var bytes = sha256.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+    return BitConverter.ToString(bytes).Replace("-", "").ToLower();
+  }
 }
